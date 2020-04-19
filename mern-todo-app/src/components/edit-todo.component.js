cb6bc09828dc779d58445ed986868ad30b8ee2be
Kelvin <kelvin.martinez92@gmail.com>
2020-04-19 18:48:30 -0400

    first push of the day

---
+++ mern-todo-app/src/components/edit-todo.component.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'; 
+import { render } from '@testing-library/react';
+
+export default class EditTodo extends Component {
+    render() {
+
+        return(
+            <div>
+                <p>Welcome To Editz Todo Component! </p>
+            </div>
+        )
+    }
+
+}
---
+++ mern-todo-app/src/components/todos-list.component.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'; 
+import { render } from '@testing-library/react';
+
+export default class TodosList extends Component {
+    render() {
+
+        return(
+            <div>
+                <p>Welcome To Todoz List Component! </p>
+            </div>
+        )
+    }
+
+}
