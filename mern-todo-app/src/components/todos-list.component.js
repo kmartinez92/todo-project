2c6ada919427cb61463c3269ea7eb208c76e1f43
Kelvin <kelvin.martinez92@gmail.com>
2020-04-20 12:05:40 -0400

    connected the front and back end

--- mern-todo-app/src/components/todos-list.component.js
+++ mern-todo-app/src/components/todos-list.component.js
@@ -1,12 +1,64 @@
 import React, {Component} from 'react'; 
-import { render } from '@testing-library/react';
+import {Link} from 'react-router-dom'; 
+import axios from 'axios'; 
+
+
+var Todo = props => (
+    <tr>
+        <td>{props.todo.todo_description}</td>
+        <td>{props.todo.todo_responsible}</td>
+        <td>{props.todo.todo_priority}</td>
+        <td>
+            <Link to={"/edit/"+props.todo._id}>Edit</Link>
+        </td>
+    </tr>
+)
 
 export default class TodosList extends Component {
+
+    constructor(props){ 
+        super(props); 
+        this.state = {todos: []}; 
+    }
+
+    //code below is requesting the information in the DB and posting it to the dom
+    componentDidMount() {
+        axios.get('http://localhost:4000/todos/')
+            .then(response => {
+                this.setState({todos: response.data}); 
+            })
+
+            .catch(function(error) {
+                console.log(error); 
+                
+            })
+    }
+
+    todoList() {
+        return this.state.todos.map(function(currentTodo, i) {
+            return <Todo todo={currentTodo} key={i} />; 
+        }); 
+    }
+
     render() {
 
         return(
             <div>
-                <p>Welcome To Todoz List Component! </p>
+                <h3>List of Todoz</h3>
+                <table className="table table-striped" style={{marginTop: 20}}>
+                    <thead>
+                        <tr>
+                            <th>Description</th>
+                            <th>Responsible</th>
+                            <th>Priority</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        { this.todoList() }
+                    </tbody>
+                </table>
             </div>
         )
     }
